c71ca03c7ffee256f6518f0232ece21645f3dbae
Manuel <mosquera.manuel2011@gmail.com>
2020-12-07 17:54:32 -0500

    Llamado a api integrado al formulario de configuración inicial

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ const container1=document.querySelector(".container1")
 const container2=document.querySelector(".container2")
 
 
-botonguardar.addEventListener("click",()=>{
+botonguardar.addEventListener("click",(e)=>{
+    e.preventDefault()
+
+    configApi()
+
     container1.style.display = "none"
     container2.style.display= "grid"
 })
---
+++ js/configAPI.js
@@ -0,0 +1,32 @@
+
+// submit formulario con la información de la configuración de la aplicación
+const configApi = () => { 
+    
+    const grillaInput = document.querySelector('#grilla')
+    const puntoInicialInput = document.querySelector('#puntoInicial')
+    const orientacionInput = document.querySelector('#orientacion')
+    const capacidadInput = document.querySelector('#capacidad')
+
+    event.preventDefault();
+
+    console.log('button submitted')
+    // fetch con post method para enviar la info al servidor
+    fetch('http://127.0.0.1:5000/config', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // definir la info que se va a enviar al servidor
+        body: JSON.stringify({
+            grilla: grillaInput.value, // tamaño de la grilla
+            puntoInicial: puntoInicialInput.value, // punto de partida
+            orientacion: orientacionInput.value, // orientacion inicial
+            capacidad: capacidadInput.value // capacidad de entrega de los drones
+        }),
+    }).then(data => data.json())
+    .then((result) => {
+        console.log('Success', result.resultado)
+    }).catch(error => {
+        console.log('ERROR', error)
+    })
+}

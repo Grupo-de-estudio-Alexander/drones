44061d3bcf3818f897d91eb21662aa628657477c
Johnm-sys <Johnm-sys.jhonmenjura12@gmail.com>
2020-12-07 23:31:02 -0500

    eliminando comentarios

--- js/entregasRealizadas.js
+++ js/entregasRealizadas.js
@@ -4,10 +4,6 @@ const llenarEntregas = async(almuerzos) => {
     tabla.innerHTML = ""
 
     const data = await createObject()
-        // for (let z = 0; z < 20; z++) {
-        //     if (z % 3 === 0) tabla.innerHTML += "<br>"
-        //     tabla.innerHTML += "<B>Dron" + (z + 1) + "</B> " + almuerzos + "&nbsp&nbsp&nbsp  "
-        // }
     data.forEach(element => {
         if (Element.id % 3 === 0) tabla.innerHTML += "<br>"
         tabla.innerHTML += "<B>Dron" + element.id + "</B> " + almuerzos + "&nbsp&nbsp&nbsp  "

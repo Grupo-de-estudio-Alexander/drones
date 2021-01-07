b6dfaedf9f796efc6b1da723ec25fc5586b1b33c
Manuel <mosquera.manuel2011@gmail.com>
2021-01-07 09:41:05 -0500

    Funcionalidad para enviar los drones agregada.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Direcciones from './Direcciones';
 import App from './app';
-import Grilla from './Grilla.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
-    <Grilla/>
   </React.StrictMode>,
   document.getElementById('root')
 );
